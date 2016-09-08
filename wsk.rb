4f65a0627e6fdc6af7dbd997abf8598dd9e65b12
James Thomas <james.thomas@uk.ibm.com>
2016-09-08 17:39:27 +0100

    Updating checksum

--- wsk.rb
+++ wsk.rb
@@ -2,7 +2,7 @@ class Wsk < Formula
   desc "openwhisk command-line utility"
   homepage "https://github.com/openwhisk/openwhisk"
   url "https://openwhisk.ng.bluemix.net/cli/go/download/mac/amd64/openwhisk-mac-amd64.tar.gz"
-  sha256 "c4c72a02510dbb7e29570f712649ef0b69ac939837ccf0528dd7f9a583dd7fb7"
+  sha256 "9d8863d28ead820678ebe82cd7370312e324518d56dd6f2cffcba4d9e991a21b"
 
   bottle :unneeded
 
